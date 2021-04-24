adff59aa5dab8841be400b3f461f5f626f7aaa93
aquaman48 <63140071+aquaman48@users.noreply.github.com>
2021-04-24 11:49:00 -0700

    Hostel-APK
    This is a basic Hostel info page. Shows an image and a button for more information of the Hostel. Once clicked user gets more information about said hostel.

---
+++ Button-Uses/YouthHostelApp/app/src/main/java/com/androidbootcamp/youthhostelapp/hostel_info.java
@@ -0,0 +1,14 @@
+package com.androidbootcamp.youthhostelapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class hostel_info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hostel_info);
+    }
+}
